eb094167c33cab4474bf14ee57653ff1c05839d7
Martin Benzo <30729285+tinchoas19@users.noreply.github.com>
2018-10-10 09:40:09 -0300

    new

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ManageProvider } from '../../providers/manage/manage';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private manage: ManageProvider) {
 
   }
 
+  hacer() {
+    this.convertToDataURLviaCanvas('https://thumbs2.ebaystatic.com/d/l225/m/meq1dL2LagH7im1peY0JtkQ.jpg', "image/jpg")
+      .then(base64Img => {
+        this.manage.saveImage(2, base64Img);
+      })
+  }
+
+  convertToDataURLviaCanvas(url, outputFormat) {
+    return new Promise((resolve, reject) => {
+      let img = new Image();
+      img.crossOrigin = 'Anonymous';
+      img.onload = function () {
+        let canvas = <HTMLCanvasElement>document.createElement('CANVAS'),
+          ctx = canvas.getContext('2d'),
+          dataURL;
+        canvas.height = 300;
+        canvas.width = 300;
+        ctx.drawImage(img, 0, 0);
+        dataURL = canvas.toDataURL(outputFormat);
+        //callback(dataURL);
+        canvas = null;
+        resolve(dataURL);
+      };
+      img.src = url;
+    });
+  }
 }
---
+++ src/providers/manage/manage.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+/*
+  Generated class for the ManageProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ManageProvider {
+ 
+
+  constructor(public http: Http) {
+    console.log('Hello ManageProvider Provider');
+  }
+  saveImage(image, idempleado) {
+
+    var url = "http://ctrlztest.com.ar/unilever/api/guardarfoto.php";
+
+    var headers = new Headers();
+    headers.append('Access-Control-Allow-Origin' , '*');
+    headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
+    headers.append('Accept','application/json');
+    headers.append('content-type','application/json');
+    const requestOptions = new RequestOptions({ headers: headers });
+
+    var body = {
+      idempleado: idempleado, 
+      foto: image
+    };
+
+    var senderBody = JSON.stringify(body);
+
+    
+    let req = this.http.post(url, senderBody, requestOptions);
+
+    req.subscribe((res) => {
+      console.log(res)
+    }, (err) => {
+      console.log(err);
+    })
+  }
+}
